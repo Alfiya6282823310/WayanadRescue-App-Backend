ebb60299ca9d28b15f45c8b3e716aade34fce171
Alfiya6282823310 <knalfiya@gmail.com>
2024-08-11 16:40:37 +0530

    Admin SignUp completed

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ const express=require("express")
 const mongoose=require("mongoose")
 const cors=require("cors")
 const bcrypt=require("bcrypt")
+const adminModel = require("./models/admin")
 
 const app=express()
 app.use(cors())
 app.use(express.json())
 
+mongoose.connect("mongodb+srv://alfiyakn:alfiyakn@cluster0.l8relji.mongodb.net/wayanadDb?retryWrites=true&w=majority&appName=Cluster0")
+app.post("/adminSignUp",(req,res)=>{
+    let input=req.body
+    let hashedPassword=bcrypt.hashSync(input.password,10)
+    console.log(hashedPassword)
+    let result=new adminModel(input)
+    result.save()
+    res.json({"status":"succes"})
+})
 
 app.listen(8080,()=>{
     console.log("server started")
